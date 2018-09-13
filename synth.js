0db8d37fad24d47039383f88296c17e151b75b9b
Jacob Albright <jacob.albright23@gmail.com>
2018-09-12 18:16:57 -0600

    started to mess around with adding a waveShaperNode for distortion. haven't arrived at anything satisfactory yet

--- synth.js
+++ synth.js
@@ -2,7 +2,7 @@ const AudioContext = window.AudioContext || window.webkitAudioContext;
 const ctx = new AudioContext();
 
 class Synth {
-  constructor(freq, waveType) {
+  constructor(freq, waveType, distortionAmount) {
     const oscillator = this.oscillator = ctx.createOscillator();
     const gain = this.gain = ctx.createGain();
     const compressor = this.compressor = ctx.createDynamicsCompressor();
@@ -11,10 +11,14 @@ class Synth {
     oscillator.frequency.setValueAtTime(freq, ctx.currentTime);
     gain.gain.setValueAtTime(0, ctx.currentTime);
 
-    oscillator.connect(compressor);
-    compressor.connect(gain);
-    gain.connect(ctx.destination);
-    oscillator.start(0);
+    if (distortionAmount) {
+      this.bringTheMud(distortionAmount);
+    } else {
+      oscillator.connect(compressor);
+      compressor.connect(gain);
+      gain.connect(ctx.destination);
+      oscillator.start(0);
+    }
   }
 
   setVolume(val, attackRelease = 0.03) {
@@ -26,15 +30,43 @@ class Synth {
     this.gain.gain.linearRampToValueAtTime(val, ctx.currentTime + attackRelease);
   }
 
+  bringTheMud(amount) {
+    const distortionGainNode = ctx.createGain();
+    const distortionNode = ctx.createWaveShaper();
+
+    distortionNode.curve = makeDistortionCurve(amount);    
+    
+    this.oscillator.connect(this.gain);
+    this.gain.connect(distortionGainNode);
+    distortionGainNode.connect(distortionNode);
+    distortionNode.connect(ctx.destination);
+    this.oscillator.start(0);
+  }
+
 }
 
 
-function mapNotes(waveType, octaveFactor, keyFreqMap = {}) {
-  console.log(waveType);
-  
+function mapNotes(waveType, octaveFactor, keyFreqMap = {}, distortionAmount) {  
   return Object.keys(keyFreqMap).reduce((accum, key) => {
-    accum[key] = new Synth(keyFreqMap[key] * octaveFactor, waveType);
+    if (distortionAmount) {
+      accum[key] = new Synth(keyFreqMap[key] * octaveFactor, waveType, distortionAmount);
+    } else {
+      accum[key] = new Synth(keyFreqMap[key] * octaveFactor, waveType);
+    }
     return accum;
   }, {});
 }
 
+function makeDistortionCurve(amount) {
+  let k = amount,
+    nSamples = 44100,
+    curve = new Float32Array(nSamples),
+    deg = Math.PI / 180,
+    i = 0,
+    x;
+  for (; i < nSamples; i++) {
+    x = i * 2 / nSamples - 1;
+    curve[i] = (30 + k) * x * 20 * deg / (Math.PI + amount * Math.abs(x));   
+  }
+  return curve;
+}
